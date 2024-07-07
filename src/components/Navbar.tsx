e6ed8cea3bc886e8eaf21312b8be84c5bfb0e842
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-07 15:07:18 +0500

    Navbar signup is done using clerk.com and homepage layout is done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 import MobileMenu from './MobileMenu'
 import Image from 'next/image'
+import { ClerkLoaded, ClerkLoading } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
 
 export default function Navbar() {
     return (
@@ -11,26 +14,55 @@ export default function Navbar() {
                 <Link href={'/'} className='font-semibold text-xl text-blue-500'>Uz<span className='font-bold text-blue-800'>BOOK</span></Link>
             </div>
             {/* center */}
-            <div className="hidden md:flex w-[50%] text-sm">
+            <div className="hidden md:flex w-[50%] text-sm items-center justify-between">
                 {/* Links */}
                 <div className="flex gap-6 text-gray-600">
                     <Link href={'/'} className='flex gap-2 items-center'>
-                        <Image src='/home.png' alt='Homepage' width={16} height={16} className='w-4 h-4'/>
+                        <Image src='/home.png' alt='Homepage' width={16} height={16} className='w-4 h-4' />
                         <span>Homepage</span>
                     </Link>
                     <Link href={'/'} className='flex gap-2 items-center'>
-                        <Image src='/friends.png' alt='Friends' width={16} height={16} className='w-4 h-4'/>
+                        <Image src='/friends.png' alt='Friends' width={16} height={16} className='w-4 h-4' />
                         <span>Friends</span>
                     </Link>
                     <Link href={'/'} className='flex gap-2 items-center'>
-                        <Image src='/stories.png' alt='Stories' width={16} height={16} className='w-4 h-4'/>
+                        <Image src='/stories.png' alt='Stories' width={16} height={16} className='w-4 h-4' />
                         <span>Stories</span>
                     </Link>
 
                 </div>
+                <div className="hidden xl:flex p-2 items-center bg-slate-100 rounded-xl ">
+                    <input type="text" placeholder='Search...' className='bg-transparent outline-none'/>
+                    <Image src='/search.png' alt='' width={20} height={20} />
+                </div>
             </div>
             {/* right */}
             <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+                <ClerkLoading>
+                    <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                        role="status">   </div>
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <SignedIn>
+                        <div className='cursor-pointer'>
+                            <Image alt='' src={'/people.png'} height={20} width={20}/>
+                        </div>
+                        <div className='cursor-pointer'>
+                            <Image alt='' src={'/messages.png'} height={20} width={20}/>
+                        </div>
+                        <div className='cursor-pointer'>
+                            <Image alt='' src={'/notifications.png'} height={20} width={20}/>
+                        </div>
+                        <UserButton />
+                    </SignedIn>
+                    <SignedOut>
+                        <div className='flex items-center gap-2 text-sm'>
+                            <Image alt='' src='/login.png' width={20} height={20}/>
+                            <Link href={'/sign-in'}>Login/Register
+                            </Link>
+                        </div>
+                    </SignedOut>
+                </ClerkLoaded>
                 <MobileMenu />
             </div>
         </div>
---
+++ src/components/RightMenu.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RightMenu() {
+  return (
+    <div>RightMenu</div>
+  )
+}
