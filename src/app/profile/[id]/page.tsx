ab28a574f9823ff5fe03956a859f70a78474ba2e
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-08 12:17:46 +0500

    ProfilePage Hero is done

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,14 +1,38 @@
 import Feed from '@/components/Feed'
 import LeftMenu from '@/components/LeftMenu'
 import RightMenu from '@/components/RightMenu'
+import Image from 'next/image'
 import React from 'react'
 
 export default function ProfilePage() {
   return (
     <div className='flex gap-6 pt-6'>
-    <div className="hidden xl:block w-[20%]"><LeftMenu /></div>
+    <div className="hidden xl:block w-[20%]"><LeftMenu type='profile' /></div>
     <div className="w-full lg:w-[70%] xl:w-[50%]">
       <div className="flex flex-col gap-6">
+        <div className="flex flex-col items-center justify-center">
+          <div className="w-full h-64 relative">
+            <Image src={'https://images.pexels.com/photos/11379609/pexels-photo-11379609.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'}
+             alt='' fill className='object-cover rounded-lg'/>
+            <Image src={'https://images.pexels.com/photos/26699373/pexels-photo-26699373/free-photo-of-a-pelican-stands-on-a-post-in-the-water.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'}
+             alt='' height={128} width={128} className='h-32 absolute w-32 object-cover left-0 right-0 -bottom-16 ring-4 ring-white rounded-full m-auto  '/>
+          </div>
+          <h1 className='mt-20 mb-4 text-2xl font-medium'>Maurice Benjamin</h1>
+          <div className="flex items-center justify-center gap-12 mb-4">
+            <div className="flex flex-col">
+              <span className='font-medium'>123</span>
+              <span className='text-sm'>Posts</span>
+            </div>
+            <div className="flex flex-col">
+              <span className='font-medium'>21K</span>
+              <span className='text-sm'>Followers</span>
+            </div>
+            <div className="flex flex-col">
+              <span className='font-medium'>1.2K</span>
+              <span className='text-sm'>Following</span>
+            </div>
+          </div>
+        </div>
         <Feed />
       </div>
     </div>
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,7 +1,32 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function ProfileCard() {
-  return (
-    <div>ProfileCard</div>
-  )
+    return (
+        <div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6'>
+            <div className="h-20 relative">
+                <Image
+                    src={'https://images.pexels.com/photos/26572408/pexels-photo-26572408/free-photo-of-silhouette-of-man-by-the-sea-during-sunset.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'}
+                    fill className='rounded-md object-cover' alt='' />
+                <Image src={'https://images.pexels.com/photos/19305502/pexels-photo-19305502/free-photo-of-woman-walking-with-umbrella-in-alley-with-colorful-stripes.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'}
+                    width={48} height={48} className='rounded-full w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10' alt='' />
+            </div>
+            <div className="h-20 flex flex-col gap-2 items-center">
+                <span className='font-semibold'>Jason White</span>
+                <div className="flex items-center gap-4">
+                    <div className="flex gap-0">
+                        <Image width={12} height={12} alt='' className='rounded-full object-cover h-3 w-3'
+                            src={'https://images.pexels.com/photos/26572408/pexels-photo-26572408/free-photo-of-silhouette-of-man-by-the-sea-during-sunset.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'} />
+                        <Image width={12} height={12} alt='' className='rounded-full'
+                            src={'https://images.pexels.com/photos/26572408/pexels-photo-26572408/free-photo-of-silhouette-of-man-by-the-sea-during-sunset.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'} />
+                        <Image width={12} height={12} alt='' className='rounded-full'
+                            src={'https://images.pexels.com/photos/26572408/pexels-photo-26572408/free-photo-of-silhouette-of-man-by-the-sea-during-sunset.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'} />
+                    </div>
+                    <span className='text-xs text-gray-500'>500 Followers</span>
+                </div>
+                <button className='bg-blue-500 text-white text-xs p-2 rounded-md'>My Profile</button>
+            </div>
+
+        </div>
+    )
 }
